ebfb89c1498bb96a37ff433d16d002c19ec9ed50
ddimitrioglo <ddimitrioglo@mitocgroup.com>
2018-07-02 18:10:49 +0300

    #THUB implement terrahub component

--- src/commands/component.js
+++ src/commands/component.js
@@ -1,6 +1,11 @@
 'use strict';
 
+const fs = require('fs');
+const fse = require('fs-extra');
+const path = require('path');
 const AbstractCommand = require('../abstract-command');
+const { templates, config } = require('../parameters');
+const { renderTwig, isAwsNameValid } = require('../helpers/util');
 
 class ComponentCommand extends AbstractCommand {
   /**
@@ -9,10 +14,37 @@ class ComponentCommand extends AbstractCommand {
   configure() {
     this
       .setName('component')
-      // .setDescription('include existing terraform folder into current project')
-      // .addOption('name', 'n', 'Component name', String)
-      // .addOption('directory', 'd', 'Path to existing component (default: cwd)', String, process.cwd())
+      .setDescription('include existing terraform folder into current project')
+      .addOption('name', 'n', 'Component name', String)
+      .addOption('parent', 'p', 'Parent component path', String, '')
+      .addOption('directory', 'd', 'Path to existing component (default: cwd)', String, process.cwd())
     ;
+  }
+
+  /**
+   * @returns {Promise}
+   */
+  run() {
+    const name = this.getOption('name');
+    const parent = this.getOption('parent');
+    const directory = path.resolve(this.getOption('directory'));
+
+    if (!isAwsNameValid(name)) {
+      throw new Error('Name is not valid, only letters, numbers, hyphens, or underscores are allowed');
+    }
+
+    const srcFile = path.join(templates.configs, 'component', `.terrahub.${config.format}.twig`);
+    const outFile = path.join(directory, config.fileName);
+
+    if (!fse.pathExistsSync(directory)) {
+      throw new Error('Can not create because path is invalid');
+    }
+
+    if (fs.existsSync(outFile)) {
+      throw new Error('Can not create because terraform component already exists');
+    }
+
+    return renderTwig(srcFile, { name: name, parent: parent }, outFile);
   }
 }
 
