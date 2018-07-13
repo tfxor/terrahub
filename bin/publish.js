5c0974857d7385528f66eec9cdfdf66ccd866f6a
mciuchitu <xscrub96x@gmail.com>
2018-07-13 17:17:21 +0300

    publish.js improved

--- bin/publish.js
+++ bin/publish.js
@@ -3,23 +3,39 @@
 'use strict';
 
 const fs = require('fs-extra');
-const HelpParser = require('../src/helpers/help-parser');
-const { templates, packageJson } = require('../src/parameters');
 const { exec } = require('child-process-promise');
 const semver = require('semver');
+const Logger = require('./../src/helpers/logger');
+const HelpParser = require('../src/helpers/help-parser');
+const { templates, packageJson } = require('../src/parameters');
+const packageContent = require(packageJson);
+
+/**
+ * Argument validation
+ */
+if (process.argv.slice(2).length !== 1) {
+  console.error('Please, enter new version as argument');
+  process.exit(1);
+}
+
+const version = process.argv[2];
+
+if (!(semver.valid(version) && semver.gt(version, packageContent.version))) {
+  console.error('New version must be in specified format and be greater than the current one.');
+  process.exit(1);
+}
 
 /**
  * @return {Promise}
  */
 function checkDiff() {
-  return exec('git diff')
-    .then(result => {
-      if (result.stdout) {
-        throw new Error('You have unstaged changes, please, commit them before publishing');
-      }
+  return exec('git diff').then(result => {
+    if (result.stdout) {
+      throw new Error('You have unstaged changes, please, commit them before publishing');
+    }
 
-      return Promise.resolve(result.stdout !== null);
-    });
+    return Promise.resolve();
+  });
 }
 
 /**
@@ -46,86 +62,68 @@ function updateHelpMetadataAndPackageJson() {
  * @return {Promise}
  */
 function commitChanges() {
-  return exec('git add . && git commit -a -m "Publish terrahub help metadata"')
-    .then(result => {
-      if (result.error) {
-        throw new Error('[Failed] to commit');
-      }
+  return exec('git add . && git commit -a -m "Publish terrahub help metadata"').then(result => {
+    if (result.error) {
+      throw new Error('[Failed] to commit');
+    }
 
-      return Promise.resolve();
-    });
+    return Promise.resolve();
+  });
 }
 
 /**
  * @return {Promise}
  */
 function deleteNodeModules() {
-  return exec('rm -rf node_modules')
-    .then(result => {
-      if (result.error) {
-        throw new Error('[Failed] cleaning up terrahub node_modules');
-      }
-
-      return Promise.resolve();
-    });
+  return fs.remove('./node_modules').catch(() => {
+    throw new Error('[Failed] cleaning up terrahub node_modules');
+  });
 }
 
 /**
  * @return {Promise}
  */
 function installNodeModules() {
-  return exec('npm install --no-shrinkwrap --no-peer')
-    .then(result => {
-      if (result.error) {
-        throw '[Failed] installing terrahub dependencies';
-      }
+  return exec('npm install --no-shrinkwrap --no-peer').then(result => {
+    if (result.error) {
+      throw '[Failed] installing terrahub dependencies';
+    }
 
-      return Promise.resolve();
-    });
+    return Promise.resolve();
+  });
 }
 
+/**
+ * @return {Promise}
+ */
 function npmVersion() {
-  return exec(`npm version ${version}`)
-    .then(result => {
-      if (result.error) {
-        throw new Error(`[Failed] updating ${version} version of terrahub package`);
-      }
+  return exec(`npm version ${version}`).then(result => {
+    if (result.error) {
+      throw new Error(`[Failed] updating ${version} version of terrahub package`);
+    }
 
-      return Promise.resolve();
-    });
+    return Promise.resolve();
+  });
 }
 
 /**
  * @return {Promise}
  */
 function gitPush() {
-  return exec('git push') // @todo: discuss where to push
-    .then(result => {
-      if (result.error) {
-        throw result.error;
-      }
+  return exec('git push').then(result => { // @todo: discuss where to push
+    if (result.error) {
+      throw result.error;
+    }
 
-      return Promise.resolve();
-    });
+    return Promise.resolve();
+  });
 }
 
 /**
  * Saves application information and commands' description in metadata.json
  * Sets the new version of the application
  */
-if (process.argv.slice(2).length !== 1) {
-  console.error('Please, enter new version as argument');
-  process.exit(1);
-}
-
-const version = process.argv[2];
-
-const packageContent = require(packageJson);
-
-if (!(semver.valid(version) && semver.gt(version, packageContent.version))) {
-  console.error('New version must be in specified format and be greater than the current one.');
-  process.exit(1);
-}
+const logger = new Logger();
 
 checkDiff()
   .then(updateHelpMetadataAndPackageJson)
@@ -135,8 +133,8 @@ checkDiff()
   .then(npmVersion)
   .then(gitPush)
   .then(() => {
-    console.log('[Ok] Done');
+    logger.info('[Ok] Done');
   })
   .catch(err => {
-    console.error(err);
+    logger.error(err);
   });
