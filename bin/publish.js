4504d15d05f8aa11a347011230a7fb10890256c5
mciuchitu <xscrub96x@gmail.com>
2018-07-16 19:04:05 +0300

    improve publish.js

--- bin/publish.js
+++ bin/publish.js
@@ -4,26 +4,10 @@
 
 const fs = require('fs-extra');
 const { exec } = require('child-process-promise');
-const semver = require('semver');
 const Logger = require('./../src/helpers/logger');
 const HelpParser = require('../src/helpers/help-parser');
 const { templates, packageJson } = require('../src/parameters');
 const packageContent = require(packageJson);
-
-/**
- * Argument validation
- */
-if (process.argv.slice(2).length !== 1) {
-  console.error('Please, enter new version as argument');
-  process.exit(1);
-}
-
-const version = process.argv[2];
-
-if (!(semver.valid(version) && semver.gt(version, packageContent.version))) {
-  console.error('New version must be in specified format and be greater than the current one.');
-  process.exit(1);
-}
 
 /**
  * @return {Promise}
@@ -32,39 +16,6 @@ function checkDiff() {
   return exec('git diff').then(result => {
     if (result.stdout) {
       throw new Error('You have unstaged changes, please, commit them before publishing');
-    }
-
-    return Promise.resolve();
-  });
-}
-
-/**
- * Updates help metadata and package.json with new version
- */
-function updateHelpMetadataAndPackageJson() {
-  const commands = HelpParser.getCommandsInstanceList();
-  packageContent.version = version;
-
-  const json = {
-    name: packageContent.name,
-    version: packageContent.version,
-    description: packageContent.description,
-    buildDate: packageContent.buildDate,
-    commands: HelpParser.getCommandsDescription(commands)
-  };
-
-  fs.writeJsonSync(templates.helpMetadata, json);
-
-  return Promise.resolve();
-}
-
-/**
- * @return {Promise}
- */
-function commitChanges() {
-  return exec('git add . && git commit -a -m "Publish terrahub help metadata"').then(result => {
-    if (result.error) {
-      throw new Error('[Failed] to commit');
     }
 
     return Promise.resolve();
@@ -107,10 +58,55 @@ function npmVersion() {
 }
 
 /**
+ * Updates help metadata and package.json with new version
+ */
+function updateHelpMetadata() {
+  const commands = HelpParser.getCommandsInstanceList();
+
+  const json = {
+    name: packageContent.name,
+    version: packageContent.version,
+    description: packageContent.description,
+    buildDate: packageContent.buildDate,
+    commands: HelpParser.getCommandsDescription(commands)
+  };
+
+  fs.writeJsonSync(templates.helpMetadata, json);
+
+  return Promise.resolve();
+}
+
+/**
+ * return {Promise}
+ */
+function npmPublish() {
+  return exec('npm publish').then(result => {
+    if (result.error) {
+      throw result.error;
+    }
+
+    return Promise.resolve();
+  });
+}
+
+/**
+ * @return {Promise}
+ */
+function commitChanges() {
+  return exec('git add . && git commit -a -m "Publish terrahub help metadata"').then(result => {
+    if (result.error) {
+      throw new Error('[Failed] to commit');
+    }
+
+    return Promise.resolve();
+  });
+}
+
+/**
  * @return {Promise}
  */
 function gitPush() {
-  return exec('git push').then(result => { // @todo: discuss where to push
+  return exec('git push').then(result => {
     if (result.error) {
       throw result.error;
     }
@@ -126,11 +122,12 @@ function gitPush() {
 const logger = new Logger();
 
 checkDiff()
-  .then(updateHelpMetadataAndPackageJson)
-  .then(commitChanges)
   .then(deleteNodeModules)
   .then(installNodeModules)
   .then(npmVersion)
+  .then(updateHelpMetadata)
+  .then(npmPublish)
+  .then(commitChanges)
   .then(gitPush)
   .then(() => {
     logger.info('[Ok] Done');
