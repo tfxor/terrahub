3a9d7f3ca648e0149475d6a6a6f5d26c8312eeee
mciuchitu <xscrub96x@gmail.com>
2018-08-03 11:23:48 +0300

    replace --json option with --output option for the build command

--- src/helpers/build-worker.js
+++ src/helpers/build-worker.js
@@ -75,13 +75,13 @@ function getComponentBuildTask(config) {
         child.stdout.on('data', data => {
           stdout.push(data);
 
-          if (process.env.json === 'false' && process.env.silent === 'false') {
+          if (!process.env.output && process.env.silent === 'false') {
             logger.raw(out(name, data));
           }
         });
 
         child.stderr.on('data', data => {
-          if (process.env.json === 'false' && process.env.silent === 'false') {
+          if (!process.env.output && process.env.silent === 'false') {
             logger.error(out(name, data));
           }
         });
@@ -89,8 +89,11 @@ function getComponentBuildTask(config) {
         return promise.then(() => Buffer.concat(stdout));
       })
     ).then(() => {
-      if (process.env.json === 'true') {
-        console.log(JSON.stringify({ message: 'Build successfully finished.' }, null, 2));
+      switch (process.env.output) {
+        case 'json': {
+          console.log(JSON.stringify({ message: 'Build successfully finished.' }, null, 2));
+          break;
+        }
       }
 
       resolve();
