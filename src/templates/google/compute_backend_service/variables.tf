59883fb0c5bcf994f55b9e1b476d554823e2b0aa
root <root@DESKTOP-ECSQPB1.localdomain>
2018-10-17 14:38:10 +0200

    add aws cognito_user_group template

--- src/templates/google/compute_backend_service/variables.tf
+++ src/templates/google/compute_backend_service/variables.tf
@@ -3,34 +3,42 @@
 #############
 # top level #
 #############
-variable "_name" {
-  description = "Name of the resource. The name must be 1-63 characters long, and comply with RFC1035. Specifically, the name must be 1-63 characters long and match the regular expression [a-z]([-a-z0-9]*[a-z0-9])? which means the first character must be a lowercase letter, and all following characters must be a dash, lowercase letter, or digit, except the last character, which cannot be a dash.."
+variable "google_compute_backend_service_name" {
+  description = "The name of the backend service."
 }
 
-variable "google_region" {
-  description = "The Region in which the created address should reside. If it is not provided, the provider region is used."
+variable "google_compute_backend_service_description" {
+  description = "Textual description for the backend."
 }
 
-variable "_description" {
-  description = "An optional description of this resource."
+variable "google_compute_backend_service_port_name" {
+  description = "The name of a service that has been added to an instance group in this backend. See related docs for details. Defaults to http."
+}
+
+variable "google_compute_backend_service_protocol" {
+  description = "The protocol for incoming requests. Defaults to HTTP."
+}
+
+variable "google_compute_backend_service_timeout_sec" {
+  description = "The number of secs to wait for a backend to respond to a request before considering the request failed. Defaults to 30."
+}
+
+variable "google_compute_backend_service_enable_cdn" {
+  description = "Whether or not to enable the Cloud CDN on the backend service."
+}
+
+variable "google_compute_backend_service_enable_cdn" {
+  description = "Whether or not to enable the Cloud CDN on the backend service."
 }
 
 variable "google_project_id" {
   description = "The ID of the project in which the resource belongs. If it is not provided, the provider project is used."
 }
 
-##########
-# labels #
-##########
-variable "custom_labels" {
-  type        = "map" 
-  description = "Custom labels. A set of key/value label pairs to assign to the project."
-  default     = {}
+variable "google_compute_backend_service_backend_group" {
+  description = "The name or URI of a Compute Engine instance group (google_compute_instance_group_manager.xyz.instance_group) that can receive traffic."
 }
 
-variable "default_labels" {
-  type        = "map" 
-  description = "Default labels. A set of key/value label pairs to assign to the project."
-  default     = {}
+variable "google_compute_backend_service_health_checks" {
+  description = "Specifies a list of HTTP/HTTPS health checks for checking the health of the backend service. Currently at most one health check can be specified, and a health check is required."
 }
-
