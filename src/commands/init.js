538cfbd5429507be38a339b3bc9d4b73c8679696
Ruslan Panevschi <panevschi.ruslan@gmail.com>
2018-11-14 15:47:23 +0200

    remove workspaceselect from init

--- src/commands/init.js
+++ src/commands/init.js
@@ -22,7 +22,7 @@ class InitCommand extends TerraformCommand {
     const distributor = new Distributor(config);
 
     return distributor
-      .runActions(['prepare', 'workspaceSelect', 'init'], {
+      .runActions(['prepare', 'init'], {
         silent: this.getOption('silent')
       }).then(() => Promise.resolve('Done'));
   }
