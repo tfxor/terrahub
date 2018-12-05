721a85017043449f0bd14647a58b1a85149ec825
mciuchitu <xscrub96x@gmail.com>
2018-12-05 17:08:51 +0200

    minor fix

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 'use strict';
 
 const { getComponentBuildTask } = require('./src/helpers/build-helper');
-const Util = require('./src/helpers/util');
 
 module.exports = {
-  getComponentBuildTask,
-  Util
+  getComponentBuildTask
 };
