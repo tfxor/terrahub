37cabeb41436d3ea31a100270b17f83e63d5768e
mciuchitu <xscrub96x@gmail.com>
2019-01-04 14:52:32 +0200

    Update src/helpers/terraform.js
    Co-Authored-By: KanoniPan <35734669+KanoniPan@users.noreply.github.com>

--- src/helpers/terraform.js
+++ src/helpers/terraform.js
@@ -201,7 +201,6 @@ class Terraform {
    * @return {Promise}
    */
   init() {
-    console.log('this', this._config);
     const promiseFunction = () => this.run('init',
       ['-no-color', this._optsToArgs({ '-input': false }), ...this._backend(), '.']);
 
