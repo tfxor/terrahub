b0d40e02fd0b1020d1842b57cef588963b363ceb
root <root@DESKTOP-ECSQPB1.localdomain>
2019-02-08 13:12:53 +0100

    replace tab with spaces

--- bin/component/main.go
+++ bin/component/main.go
@@ -1,128 +1,128 @@
 package main
 
 import (
-	"bytes"
-	"flag"
-	"fmt"
-	"io/ioutil"
-	"os"
-	"os/exec"
-	"regexp"
-	"strings"
+  "bytes"
+  "flag"
+  "fmt"
+  "io/ioutil"
+  "os"
+  "os/exec"
+  "regexp"
+  "strings"
 
-	"github.com/hashicorp/hcl/hcl/printer"
-	jsonParser "github.com/hashicorp/hcl/json/parser"
+  "github.com/hashicorp/hcl/hcl/printer"
+  jsonParser "github.com/hashicorp/hcl/json/parser"
 )
 
 // VERSION is what is returned by the `-v` flag
 var Version = "development"
 
 func main() {
-	version := flag.Bool("version", false, "Prints current app version")
-	flag.Parse()
-	if *version {
-		fmt.Println(Version)
-		return
-	}
+  version := flag.Bool("version", false, "Prints current app version")
+  flag.Parse()
+  if *version {
+    fmt.Println(Version)
+    return
+  }
 
-	argsWithoutProg := os.Args[1:]
-	terrahubComponent := ""
-	terrahubComponentPath := ""
-	cashPath := ""
-	if len(argsWithoutProg) > 0 {
-		cashPath = os.Args[1]
-	}
-	if len(argsWithoutProg) > 1 {
-		terrahubComponentPath = os.Args[2]
-	}
-	if len(argsWithoutProg) > 2 {
-		terrahubComponent = os.Args[3]
-	}
-	cashComponentPath := PrepareJSON(terrahubComponent)
-	if cashComponentPath != "" {
-		GenerateHcl(cashPath+"/"+cashComponentPath+"/", terrahubComponentPath)
-	}
+  argsWithoutProg := os.Args[1:]
+  terrahubComponent := ""
+  terrahubComponentPath := ""
+  cashPath := ""
+  if len(argsWithoutProg) > 0 {
+    cashPath = os.Args[1]
+  }
+  if len(argsWithoutProg) > 1 {
+    terrahubComponentPath = os.Args[2]
+  }
+  if len(argsWithoutProg) > 2 {
+    terrahubComponent = os.Args[3]
+  }
+  cashComponentPath := PrepareJSON(terrahubComponent)
+  if cashComponentPath != "" {
+    GenerateHcl(cashPath+"/"+cashComponentPath+"/", terrahubComponentPath)
+  }
 
 }
 
 func PrepareJSON(terrahubComponent string) string {
-	var (
-		cmdOut []byte
-		err    error
-	)
-	cmdName := "terrahub"
-	cmdArgs := []string{"prepare", "-i", terrahubComponent}
-	if cmdOut, err = exec.Command(cmdName, cmdArgs...).Output(); err != nil {
-		fmt.Fprintln(os.Stderr, "There was an error running terrahub rev-parse command: ", err)
-		os.Exit(1)
-	}
-	sha := string(cmdOut)
-	re := regexp.MustCompile(`(?m).+?✅`)
-	for _, match := range re.FindAllString(sha, -1) {
-		match = strings.Replace(match, "✅", "", -1)
-		return match
-	}
-	return ""
+  var (
+    cmdOut []byte
+    err    error
+  )
+  cmdName := "terrahub"
+  cmdArgs := []string{"prepare", "-i", terrahubComponent}
+  if cmdOut, err = exec.Command(cmdName, cmdArgs...).Output(); err != nil {
+    fmt.Fprintln(os.Stderr, "There was an error running terrahub rev-parse command: ", err)
+    os.Exit(1)
+  }
+  sha := string(cmdOut)
+  re := regexp.MustCompile(`(?m).+?✅`)
+  for _, match := range re.FindAllString(sha, -1) {
+    match = strings.Replace(match, "✅", "", -1)
+    return match
+  }
+  return ""
 }
 
 func GenerateHcl(sourcePath string, destinationPath string) {
-	f, err := os.Open(sourcePath)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fileInfo, err := f.Readdir(-1)
-	f.Close()
-	if err != nil {
-		fmt.Println(err)
-	}
+  f, err := os.Open(sourcePath)
+  if err != nil {
+    fmt.Println(err)
+  }
+  fileInfo, err := f.Readdir(-1)
+  f.Close()
+  if err != nil {
+    fmt.Println(err)
+  }
 
-	for _, file := range fileInfo {
-		if !file.IsDir() {
-			StartProccesingFile(sourcePath+file.Name(), destinationPath+"/"+file.Name())
-		}
-	}
-	ProccesingDotTerrahub(destinationPath + "/.terrahub.yml")
+  for _, file := range fileInfo {
+    if !file.IsDir() {
+      StartProccesingFile(sourcePath+file.Name(), destinationPath+"/"+file.Name())
+    }
+  }
+  ProccesingDotTerrahub(destinationPath + "/.terrahub.yml")
 }
 
 func StartProccesingFile(source string, destination string) {
-	input, _ := ioutil.ReadFile(source)
-	sourceValue := strings.Replace(string(input), "null", "", -1)
-	sourceValue = strings.Replace(sourceValue, "NULL", "", -1)
-	// fmt.Println(string(input))
-	ast, err := jsonParser.Parse([]byte(sourceValue))
-	if err != nil {
-		fmt.Printf("unable to parse JSON: %s", err)
-	}
-	var b bytes.Buffer
-	err = printer.Fprint(&b, ast)
-	if err != nil {
-		fmt.Printf("unable to print HCL: %s", err)
-	}
-	ioutil.WriteFile(destination, Clearing(b.String()), 0777)
+  input, _ := ioutil.ReadFile(source)
+  sourceValue := strings.Replace(string(input), "null", "", -1)
+  sourceValue = strings.Replace(sourceValue, "NULL", "", -1)
+  // fmt.Println(string(input))
+  ast, err := jsonParser.Parse([]byte(sourceValue))
+  if err != nil {
+    fmt.Printf("unable to parse JSON: %s", err)
+  }
+  var b bytes.Buffer
+  err = printer.Fprint(&b, ast)
+  if err != nil {
+    fmt.Printf("unable to print HCL: %s", err)
+  }
+  ioutil.WriteFile(destination, Clearing(b.String()), 0777)
 }
 
 func Clearing(input string) []byte {
-	input = strings.Replace(input, "\"resource\"", "resource", -1)
-	input = strings.Replace(input, "\"data\"", "data", -1)
-	input = strings.Replace(input, "\"output\"", "output", -1)
-	input = strings.Replace(input, "\"module\"", "module", -1)
-	input = strings.Replace(input, "\"provider\"", "provider", -1)
-	input = strings.Replace(input, "\"backend\"", "backend", -1)
-	input = strings.Replace(input, "\"variable\"", "variable", -1)
-	re := regexp.MustCompile(`(?m)\".+?\" =`)
-	for _, match := range re.FindAllString(input, -1) {
-		input = strings.Replace(input, match, strings.Replace(match, "\"", "", -1), -1)
-	}
-	return []byte(input)
+  input = strings.Replace(input, "\"resource\"", "resource", -1)
+  input = strings.Replace(input, "\"data\"", "data", -1)
+  input = strings.Replace(input, "\"output\"", "output", -1)
+  input = strings.Replace(input, "\"module\"", "module", -1)
+  input = strings.Replace(input, "\"provider\"", "provider", -1)
+  input = strings.Replace(input, "\"backend\"", "backend", -1)
+  input = strings.Replace(input, "\"variable\"", "variable", -1)
+  re := regexp.MustCompile(`(?m)\".+?\" =`)
+  for _, match := range re.FindAllString(input, -1) {
+    input = strings.Replace(input, match, strings.Replace(match, "\"", "", -1), -1)
+  }
+  return []byte(input)
 }
 
 func ProccesingDotTerrahub(source string) {
-	input, _ := ioutil.ReadFile(source)
-	endIndex := strings.Index(string(input), "  template:")
-	startIndex := strings.Index(string(input), "## build config")
-	sourceValue := string(input)[:endIndex]
-	if startIndex > -1 {
-		sourceValue += "\n" + string(input)[startIndex:]
-	}
-	ioutil.WriteFile(source, []byte(sourceValue), 0777)
+  input, _ := ioutil.ReadFile(source)
+  endIndex := strings.Index(string(input), "  template:")
+  startIndex := strings.Index(string(input), "## build config")
+  sourceValue := string(input)[:endIndex]
+  if startIndex > -1 {
+    sourceValue += "\n" + string(input)[startIndex:]
+  }
+  ioutil.WriteFile(source, []byte(sourceValue), 0777)
 }
--- bin/generator/terraform/generator.go
+++ bin/generator/terraform/generator.go
@@ -1,73 +1,73 @@
 package terraform
 
 import (
-	"encoding/json"
-	"fmt"
-	"os"
-	"strings"
+  "encoding/json"
+  "fmt"
+  "os"
+  "strings"
 )
 
 // Generation - Generation templates from go files
 func Generation(provider string, source string, destination string) {
-	fmt.Println("Start reading path: " + source)
-	f, err := os.Open(source)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fileInfo, err := f.Readdir(-1)
-	f.Close()
-	if err != nil {
-		fmt.Println(err)
-	}
+  fmt.Println("Start reading path: " + source)
+  f, err := os.Open(source)
+  if err != nil {
+    fmt.Println(err)
+  }
+  fileInfo, err := f.Readdir(-1)
+  f.Close()
+  if err != nil {
+    fmt.Println(err)
+  }
 
-	for _, file := range fileInfo {
-		if strings.Index(file.Name(), "resource_") > -1 &&
-			strings.Index(file.Name(), "_test") == -1 {
-			fmt.Println("Read file: " + file.Name())
-			StartProccesingFile(source, destination, file.Name(), provider)
-		}
-	}
+  for _, file := range fileInfo {
+    if strings.Index(file.Name(), "resource_") > -1 &&
+      strings.Index(file.Name(), "_test") == -1 {
+      fmt.Println("Read file: " + file.Name())
+      StartProccesingFile(source, destination, file.Name(), provider)
+    }
+  }
 }
 
 // StartProccesingFile - Start proccesing
 func StartProccesingFile(source string, destination string, fileName string, provider string) {
-	err, result, existID := ParsingFile(source, fileName, provider)
+  err, result, existID := ParsingFile(source, fileName, provider)
 
-	if err {
-		fmt.Println("=====Error=====")
-		fmt.Println("Provider: " + provider)
-		fmt.Println("File: " + fileName)
-		fmt.Println("Error: " + result)
-		return
-	}
+  if err {
+    fmt.Println("=====Error=====")
+    fmt.Println("Provider: " + provider)
+    fmt.Println("File: " + fileName)
+    fmt.Println("Error: " + result)
+    return
+  }
 
-	jsonMap := make(map[string]interface{})
-	errJson := json.Unmarshal([]byte(result), &jsonMap)
-	if errJson != nil {
-		panic(errJson)
-	}
-	fileName = fileName[0 : len(fileName)-3]
-	fileName = strings.Replace(fileName, "resource_", "", -1)
-	fileName = strings.Replace(fileName, provider+"_", "", -1)
-	fileName = strings.Replace(fileName, ".go", "", -1)
-	templatePath := destination + provider + "/" + fileName + "/.terrahub.yml.twig"
-	if _, err := os.Stat(templatePath); os.IsNotExist(err) {
-		merr := os.MkdirAll(destination+provider+"/"+fileName, os.ModePerm)
-		if merr != nil {
-			panic(merr)
-		}
-		_, cerr := os.Create(templatePath)
-		if cerr != nil {
-			fmt.Println(cerr)
-		}
-		fmt.Println("Generation template: " + templatePath)
-		// fmt.Println(existID)
-		GenerateYml(
-			GenerateYmlParam{
-				templatePath,
-				jsonMap,
-				provider + "_" + fileName,
-				existID,
-				provider})
-	}
+  jsonMap := make(map[string]interface{})
+  errJson := json.Unmarshal([]byte(result), &jsonMap)
+  if errJson != nil {
+    panic(errJson)
+  }
+  fileName = fileName[0 : len(fileName)-3]
+  fileName = strings.Replace(fileName, "resource_", "", -1)
+  fileName = strings.Replace(fileName, provider+"_", "", -1)
+  fileName = strings.Replace(fileName, ".go", "", -1)
+  templatePath := destination + provider + "/" + fileName + "/.terrahub.yml.twig"
+  if _, err := os.Stat(templatePath); os.IsNotExist(err) {
+    merr := os.MkdirAll(destination+provider+"/"+fileName, os.ModePerm)
+    if merr != nil {
+      panic(merr)
+    }
+    _, cerr := os.Create(templatePath)
+    if cerr != nil {
+      fmt.Println(cerr)
+    }
+    fmt.Println("Generation template: " + templatePath)
+    // fmt.Println(existID)
+    GenerateYml(
+      GenerateYmlParam{
+        templatePath,
+        jsonMap,
+        provider + "_" + fileName,
+        existID,
+        provider})
+  }
 }
