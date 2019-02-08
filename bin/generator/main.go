a9f7e11686d26612103da622af99799a8361ffdc
root <root@DESKTOP-ECSQPB1.localdomain>
2019-02-08 13:20:46 +0100

    fix codeclimate issues

--- bin/generator/main.go
+++ bin/generator/main.go
@@ -1,72 +1,72 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "os"
+	"flag"
+	"fmt"
+	"os"
 
-  "generator/proccesing"
+	"generator/terraform"
 )
 
 // VERSION is what is returned by the `-v` flag
 var Version = "development"
 
 func main() {
-  version := flag.Bool("version", false, "Prints current app version")
-  toYml := flag.Bool("toyml", false, "Input HCL folder, output yml file")
-  flag.Parse()
-  if *version {
-    fmt.Println(Version)
-    return
-  }
+	version := flag.Bool("version", false, "Prints current app version")
+	toYml := flag.Bool("toyml", false, "Input HCL folder, output yml file")
+	flag.Parse()
+	if *version {
+		fmt.Println(Version)
+		return
+	}
 
-  if *toYml {
-    ConvertToYml()
-    return
-  }
+	if *toYml {
+		ConvertToYml()
+		return
+	}
 
-  GenerationTemplates()
+	GenerationTemplates()
 }
 
 // GenerationTemplates - Generation templates files
 func GenerationTemplates() {
-  argsWithoutProg := os.Args[1:]
-  providers := []string{"aws", "google", "azurerm"}
-  destination := "./templates/"
-  if len(argsWithoutProg) > 0 {
-    providers = []string{os.Args[1]}
-  }
-  if len(argsWithoutProg) > 1 {
-    destination = os.Args[2]
-  }
-  for _, provider := range providers {
-    downloaded := proccesing.DownloadFromGit(
-      "https://github.com/terraform-providers/terraform-provider-" + provider + ".git")
+	argsWithoutProg := os.Args[1:]
+	providers := []string{"aws", "google", "azurerm"}
+	destination := "./templates/"
+	if len(argsWithoutProg) > 0 {
+		providers = []string{os.Args[1]}
+	}
+	if len(argsWithoutProg) > 1 {
+		destination = os.Args[2]
+	}
+	for _, provider := range providers {
+		downloaded := terraform.DownloadFromGit(
+			"https://github.com/terraform-providers/terraform-provider-" + provider + ".git")
 
-    if downloaded {
-      proccesing.DeleteFolder(destination + provider)
-      proccesing.Generation(provider, "./terraform-provider-"+provider+"/"+provider+"/", destination)
-      proccesing.DeleteFolder("./terraform-provider-" + provider)
-    }
-  }
+		if downloaded {
+			terraform.DeleteFolder(destination + provider)
+			terraform.Generation(provider, "./terraform-provider-"+provider+"/"+provider+"/", destination)
+			terraform.DeleteFolder("./terraform-provider-" + provider)
+		}
+	}
 }
 
 // ConvertToYml - Cnvert tf to yml
 func ConvertToYml() {
-  argsWithoutProg := os.Args[2:]
-  source := ""
-  destination := "./terrahub/"
-  if len(argsWithoutProg) > 0 {
-    source = os.Args[2]
-  }
-  if len(argsWithoutProg) > 1 {
-    destination = os.Args[3]
-  } else {
-    destination = source
-  }
-  if source == "" {
-    fmt.Println("The source path is not set!")
-  } else {
-    proccesing.ParsingTfFile(source, destination)
-  }
+	argsWithoutProg := os.Args[2:]
+	source := ""
+	destination := "./terrahub/"
+	if len(argsWithoutProg) > 0 {
+		source = os.Args[2]
+	}
+	if len(argsWithoutProg) > 1 {
+		destination = os.Args[3]
+	} else {
+		destination = source
+	}
+	if source == "" {
+		fmt.Println("The source path is not set!")
+	} else {
+		terraform.ParsingTfFile(source, destination)
+	}
 }
