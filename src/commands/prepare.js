ef27ae06388cedb6103f88efab5736f3441bc33a
mciuchitu <xscrub96x@gmail.com>
2019-03-14 18:48:13 +0200

    PrepareCommand minor improvement

--- src/commands/prepare.js
+++ src/commands/prepare.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const logger = require('../helpers/logger');
 const TerraformCommand = require('../terraform-command');
 const Distributor = require('../helpers/distributors/thread-distributor');
 
@@ -21,14 +20,16 @@ class PrepareCommand extends TerraformCommand {
   run() {
     const config = this.getConfigObject();
     const distributor = new Distributor(config);
+
     if (!this.getOption('silent')) {
       const firstKey = Object.keys(config)[0];
-      logger.raw(firstKey);
+
+      this.logger.log(firstKey);
     }
-    return distributor
-      .runActions(['prepare'], {
-        silent: this.getOption('silent')
-      }).then(() => Promise.resolve('Done'));
+
+    return distributor.runActions(['prepare'], {
+      silent: this.getOption('silent')
+    }).then(() => Promise.resolve('Done'));
   }
 }
 
