0588c03aa0b3488a603ca4af8976e9f2c641348a
Eugene I <eugene@mitocgroup.com>
2019-05-24 08:47:55 -0400

    improve message

--- src/terraform-command.js
+++ src/terraform-command.js
@@ -30,7 +30,7 @@ class TerraformCommand extends AbstractCommand {
       .addOption('git-diff', 'g', 'List of components to include (git diff)', Array, [])
       .addOption('var', 'r', 'Variable(s) to be used by terraform', Array, [])
       .addOption('var-file', 'l', 'Variable file(s) to be used by terraform', Array, [])
-      .addOption('dependency', 'D', 'Set TerraHub dependency validation strategy', String, 'auto')
+      .addOption('dependency', 'D', 'Set dependency validation strategy (auto, ignore, include)', String, 'auto')
       ;
   }
 
