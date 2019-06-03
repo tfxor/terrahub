b2f896e5433932cea65d54610018c32b70061faf
root <root@DESKTOP-ECSQPB1.localdomain>
2019-06-03 16:05:56 +0200

    minor changes

--- src/helpers/jit-helper.js
+++ src/helpers/jit-helper.js
@@ -51,9 +51,10 @@ class JitHelper {
    * @private
    */
   static _normalizeBackendLocalPath(config) {
-    const owner = config.owner || '';
-    const localTfstatePath = path.resolve('/tmp/.terrahub/local_tfstate/', owner, config.project.name);
-    const { template: { terraform: { backend: { local } } } } = config;
+    const { template } = config;
+    const { terraform: { backend: { local } } } = template;
+    const localTfstatePath = template.locals.component.local ||
+      path.resolve('/tmp/.terrahub/local_tfstate/', config.project.name);
 
     if (local) {
       Object.keys(local).filter(it => local[it]).map(() => {
@@ -74,9 +75,10 @@ class JitHelper {
    * @private
    */
   static _normalizeBackendS3Key(config) {
-    const owner = config.owner || '';
-    const remoteTfstatePath = path.join('terraform', owner, config.project.name);
-    const { template: { terraform: { backend: { s3 } } } } = config;
+    const { template } = config;
+    const { terraform: { backend: { s3 } } } = template;
+    const remoteTfstatePath = template.locals.component.remote ||
+      path.join('terraform', config.project.name);
 
     if (s3) {
       Object.keys(s3).filter(it => s3[it]).map(() => {
