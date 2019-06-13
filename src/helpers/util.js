134df87ded8c2c172fe190b70777ff923f6bfb8f
aluchianic <andrey.luchianic@gmail.com>
2019-06-13 17:19:59 +0300

    fix error message in import

--- src/helpers/util.js
+++ src/helpers/util.js
@@ -234,9 +234,11 @@ class Util {
     return promise.then(() => Buffer.concat(stdout)).catch(err => {
       err.message = Buffer.concat(stderr).toString();
 
-      return Promise.reject('Error occurred. Please try again. If this problem persists, ' +
-        'enable extra debugging (DEBUG=debug) to see more details and open an issue at ' +
-        'https://github.com/TerraHubCorp/terrahub/issues');
+      if (err) {
+        return Promise.reject('Error occurred. Please try again. If this problem persists, ' +
+          'enable extra debugging (DEBUG=debug) to see more details and open an issue at ' +
+          'https://github.com/TerraHubCorp/terrahub/issues');
+      }
     });
   }
 
