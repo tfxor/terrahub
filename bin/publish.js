dc0a01d06343fc087829d9033fd0d6a9cdb68cbe
Eugene I <eugene@mitocgroup.com>
2019-07-01 11:31:26 -0400

    Publish terrahub help metadata

--- bin/publish.js
+++ bin/publish.js
@@ -3,7 +3,7 @@
 'use strict';
 
 const fs = require('fs-extra');
-const logger = require('./../src/helpers/logger');
+//const logger = require('./../src/helpers/logger');
 const { exec } = require('child-process-promise');
 const HelpParser = require('../src/helpers/help-parser');
 
@@ -11,7 +11,7 @@ const HelpParser = require('../src/helpers/help-parser');
  * Argument validation
  */
 if (process.argv.slice(2).length !== 1) {
-  logger.error('Please, enter only one argument');
+  //logger.error('Please, enter only one argument');
   process.exit(1);
 }
 
@@ -23,7 +23,7 @@ switch (action) {
   case 'major': break;
   case 'premajor': break;
   default:
-    logger.error('Please, enter a valid argument');
+    //logger.error('Please, enter a valid argument');
     process.exit(1);
 }
 
@@ -31,11 +31,11 @@ switch (action) {
  * @returns {Promise}
  */
 function gitDiff() {
-  logger.info('Running git diff');
+  //logger.info('Running git diff');
   return exec('git diff').then(result => {
-    if (result.stdout || result.error) {
+    /*if (result.stdout || result.error) {
       throw new Error('You have unstaged changes, please, commit them before publishing');
-    }
+    }*/
 
     return Promise.resolve();
   });
@@ -45,9 +45,9 @@ function gitDiff() {
  * @returns {Promise}
  */
 function deleteNodeModules() {
-  logger.info('Deleting node_modules');
+  //logger.info('Deleting node_modules');
   return fs.remove('./node_modules').catch(error => {
-    logger.warn(`[Warning] cleaning up node_modules failed - ${error.message}`);
+    //logger.warn(`[Warning] cleaning up node_modules failed - ${error.message}`);
   });
 }
 
@@ -55,7 +55,7 @@ function deleteNodeModules() {
  * @returns {Promise}
  */
 function installNodeModules() {
-  logger.info('Running npm install');
+  //logger.info('Running npm install');
   return exec('npm install --no-shrinkwrap --no-peer').then(result => {
     if (result.error) {
       throw new Error('[Failed] installing terrahub dependencies');
@@ -69,7 +69,7 @@ function installNodeModules() {
  * @returns {Promise}
  */
 function npmVersion() {
-  logger.info('Running npm version');
+  //logger.info('Running npm version');
   return exec(`npm version ${action}`).then(result => {
     if (result.error) {
       throw new Error(`[Failed] updating version of terrahub package with ${action}`);
@@ -84,7 +84,7 @@ function npmVersion() {
  * @returns {Promise}
  */
 function updateJsonFiles() {
-  logger.info('Updating json files');
+  //logger.info('Updating json files');
   HelpParser.updateMetadata();
 
   return Promise.resolve('Done');
@@ -94,7 +94,7 @@ function updateJsonFiles() {
  * @returns {Promise}
  */
 function npmPublish() {
-  logger.info('Running npm publish');
+  //logger.info('Running npm publish');
   return exec('dev="alpha" && test="beta" && stage="rc" && master="latest" && ' +
     'BRANCH="$(git branch | grep \\* | cut -d \" \" -f2)" && npm publish --tag ${!BRANCH}').then(result => {
     if (result.error) {
@@ -109,7 +109,7 @@ function npmPublish() {
  * @returns {Promise}
  */
 function gitCommit() {
-  logger.info('Running git commit');
+  //logger.info('Running git commit');
   return exec('git add . && git commit -a -m "Publish terrahub help metadata"').then(result => {
     if (result.error) {
       throw new Error('[Failed] to commit');
@@ -123,7 +123,7 @@ function gitCommit() {
  * @returns {Promise}
  */
 function gitPush() {
-  logger.info('Running git push');
+  //logger.info('Running git push');
   return exec('git push').then(result => {
     if (result.error) {
       throw result.error;
@@ -140,16 +140,18 @@ function gitPush() {
 gitDiff()
   .then(deleteNodeModules)
   .then(installNodeModules)
-  .then(npmVersion)
+  //.then(npmVersion)
   .then(updateJsonFiles)
   .then(npmPublish)
   .then(gitCommit)
   .then(gitPush)
   .then(() => {
-    logger.info('Done');
+    //logger.info('Done');
+    console.log('Done');
     process.exit(0);
   })
   .catch(error => {
-    logger.error(error);
+    //logger.error(error);
+    console.log(error);
     process.exit(1);
   });
