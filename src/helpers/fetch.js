2e4db0505f55b5ab4cee22001b5879988ec16c36
aluchianic <andrey.luchianic@gmail.com>
2019-07-08 15:07:41 +0300

    jenkins debug api requests

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -79,6 +79,12 @@ class Fetch {
       //   body: json
       // }, null, 2));
 
+      console.log({
+          url: result.url,
+          status: result.status,
+          body: json
+      });
+
       let error;
       switch (result.status) {
         case 403:
