3e67053ecd3e94f18c28f821b1250e2633954312
Eugene I <eugene@mitocgroup.com>
2020-06-08 15:23:51 -0400

    improve lambda

--- src/templates/aws/lambda_function/scripts/shasum.sh
+++ src/templates/aws/lambda_function/scripts/shasum.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 ## Source path
 THUB_SRC=${1}
 if [ -z "${THUB_SRC}" ]; then
-  echo 'ERROR: THUB_SRC variable is empty. Aborting...'
+  echo '[ERROR]: THUB_SRC variable is empty. Aborting...'
   exit 1
 fi
 
@@ -11,15 +11,16 @@ fi
 [ -f .terrahub_build.env ] && . .terrahub_build.env
 
 ## Checking if THUB_BUILD_OK is true
-if [ "$THUB_BUILD_OK" == "true" ]; then
-  ## Checking if SHA256 sums exists
-  if [ -z "${THUB_SHA}" ]; then
-    echo "ERROR: THUB_SHA variable is empty. Aborting..."
-    exit 1
-  fi
-
-  ## Write current SHA256 to THUB_SRC
-  echo "${THUB_SHA}" > ${THUB_SRC}
-else
-  echo "Build was NOT executed ==> SHA256 will NOT be updated."
+if [ "$THUB_BUILD_OK" != "true" ]; then
+  echo "[INFO]: Build was NOT executed ==> SHA256 will NOT be updated."
+  exit 0
 fi
+
+## Checking if SHA256 sums exists
+if [ -z "${THUB_SHA}" ]; then
+  echo "[ERROR]: THUB_SHA variable is empty. Aborting..."
+  exit 1
+fi
+
+## Write current SHA256 to THUB_SRC
+echo "${THUB_SHA}" > ${THUB_SRC}
