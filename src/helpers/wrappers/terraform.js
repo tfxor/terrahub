218c9f546122fb2b9ea510ec921367f797928d2f
Vitalie Calmîc <vcalmic@mitocgroup.com>
2020-10-19 20:10:42 +0300

    update condition

--- src/helpers/wrappers/terraform.js
+++ src/helpers/wrappers/terraform.js
@@ -80,7 +80,7 @@ class Terraform {
       ? this._config.template.provider[0][provider].profile || 'default'
       : this._config.template.provider[provider].profile || 'default';
 
-    if (accounts.length && ApiHelper.tokenIsValid) {
+    if (accounts.length > 0 && configs.length === 0) {
       cloudAccounts = await ApiHelper.retrieveCloudAccounts();
     }
     if (configs.length > 0) {
